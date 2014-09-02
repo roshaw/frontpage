e85202d1096d0345d85ef5cec5240a98d82f7c3f
Rostislav Kirchev <rostislav.kirchev@gmail.com>
2014-09-02 23:18:18 +0300

    FrontPage

---
+++ js/script.js
@@ -0,0 +1,54 @@
+﻿$(document).ready(function () {
+
+    var $searchShopWrap = $('#search-shop-wrap');
+    var $mainNav = $('#main-nav');
+
+    //Click on the search icon
+    $searchShopWrap.find('.search-icon-header').on('click', function () {
+
+        if ($(this).hasClass('active')) {
+            //close search bar
+            $(this).removeClass('active');
+        } else {
+
+            //remove active class from all sub-menus
+            $mainNav.find('.has-sub-menu').removeClass('active');
+            $mainNav.find('.sub-menu').removeClass('active');
+
+            //open search bar
+            $(this).addClass('active');
+        }   
+    });
+
+    //Click on the close search icon
+    $searchShopWrap.find('.close-search').on('click', function () {
+        $searchShopWrap.find('.search-icon-header').removeClass('active');
+    });
+
+    //click on the menu element with sub-menu
+    $mainNav.find('.has-sub-menu').on('click', function () {
+
+        var theClass = $(this).attr("class").match(/has-sub-menu-[\w-]*\b/);
+        var menuClass = theClass[0].substr(4);
+
+        if ($(this).hasClass('active')) {
+
+            //remove active class from element and sub-menu
+            $(this).removeClass('active');
+            $mainNav.find('.' + menuClass).removeClass('active');
+
+        } else {
+            //close search bar if it's open
+            $searchShopWrap.find('.search-icon-header').removeClass('active');
+
+            //remove active class from all elements
+            $mainNav.find('.has-sub-menu').removeClass('active');
+            $mainNav.find('.sub-menu').removeClass('active');
+
+            //add active class to element and apropriate sub-menu
+            $(this).addClass('active');
+            $mainNav.find('.' + menuClass).addClass('active');
+        }
+        return false;
+    });
+});
